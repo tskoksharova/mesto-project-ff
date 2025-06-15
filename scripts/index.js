51ac56fdf699855ea0ee0efa8794867529ebcf62
tskoksharova <tskoksharova@gmail.com>
2025-06-15 19:19:57 +0500

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard(cardData){
+    const cardElement = cardTemplate.cloneNode(true);
+    const card = cardElement.querySelector('.card');
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        card.remove();
+    })
+    return cardElement;
+}
+
+function renderInitialCards(){
+    initialCards.forEach((cardData) => {
+        const cardElement = createCard(cardData);
+        cardsContainer.appendChild(cardElement);
+    })
+}
+
+renderInitialCards();
